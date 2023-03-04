1a9ea8c1429eefef73a2f789edd5b02961ea3774
Traceur0 <ghost01ghost@gmail.com>
2023-03-04 23:58:26 +0900

    localStorage part

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const signInInput = document.querySelector("#sign-in-form input");
-const signInBtn = document.querySelector("#sign-in-form button");
+const signinForm = document.querySelector("#signin-form");
+const signinInput = document.querySelector("#signin-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN = "hidden";
+const USERNAME = "";
 
 
-function onSignInBtnClick() {
-    const name = signInInput.value;
-    const name_len = name.length;
-    // name 유효성 검사
-    if (name_len <= 2) {
-        alert("이름은 3자 이상이어야 합니다");
-    } else if (name_len >= 15) {
-        alert("이름은 15자 미만이어야 합니다");
-    } else {
-        console.log(name);
-    }
+function onSigninBtnClick(event) {
+    event.preventDefault();
+    signinForm.classList.add(HIDDEN);
+    const username = signinInput.value;
+    localStorage.setItem('username', username);   
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN);
 }
 
+signinForm.addEventListener("submit", onSigninBtnClick);
 
-signInBtn.addEventListener("click", onSignInBtnClick);
+
+// if reload page
+const savedUsername = localStorage.getItem(USERNAME);
+
+if (savedUsername === null) {
+    onSigninBtnClick();
+} else {
+    signinForm.classList.add(HIDDEN);
+    onSigninBtnClick();
+}
