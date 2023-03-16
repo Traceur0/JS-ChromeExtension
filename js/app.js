faf8fc9c67b3a2b6a1d821104a364c16e83ccd34
Traceur0 <ghost01ghost@gmail.com>
2023-03-16 17:21:52 +0900

    username part end

--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
-const signInInput = document.querySelector("#sign-in-form input");
-const signInBtn = document.querySelector("#sign-in-form button");
+// HTML요소 JS로 변수 할당
+const signinForm = document.querySelector("#sign-in-form");
+const signinInput = document.querySelector("#sign-in-form input");
+const greeting = document.querySelector("#greeting");
 
-function onSignInBtnClick() {
-  const name = signInInput.value;
-  console.log(name);
+// 자주 쓰이는 STRING KEYWORD 지정
+const CLASSNAME_HIDDEN = "hidden";
+const KEY_USERNAME = "username";
+
+// 버튼 입력시에 작동하는 동작
+// Default Action
+function onSigninBtnSubmit(event) {
+  event.preventDefault();
+  signinForm.classList.add(CLASSNAME_HIDDEN);
+  const username = signinInput.value;
+  localStorage.setItem(KEY_USERNAME, username);
+  createGreeting();
 }
 
-signInBtn.addEventListener("click", onSignInBtnClick);
+// H1(greeting) 생성 함수
+function createGreeting() {
+  const username = localStorage.getItem(KEY_USERNAME);
+  greeting.innerText = `name: ${username}`;
+  greeting.classList.remove(CLASSNAME_HIDDEN);
+}
 
-signInBtn.addEventListener("");
+// 브라우저 로컬스토리지 변수 JS로 변수 할당
+const savedUsername = localStorage.getItem(KEY_USERNAME);
+
+// STATUS 확인 후 상황에 따른 동작 설정
+if (savedUsername === null) {
+  signinForm.classList.remove(CLASSNAME_HIDDEN);
+  signinForm.addEventListener("submit", onSigninBtnSubmit);
+} else {
+  createGreeting();
+}
